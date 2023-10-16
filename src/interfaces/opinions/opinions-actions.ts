1c3f36809a195cf552b319c8dbfdb8475cf63b83
Amos <asidelnik@gmail.com>
2023-10-16 14:20:14 +0300

    context - update

--- src/interfaces/opinions/opinions-actions.ts
+++ src/interfaces/opinions/opinions-actions.ts
@@ -1,30 +1,19 @@
-import { OpinionsGroup } from "../interfaces";
-
-// type Reset = { type: "reset" };
-
-type UploadExcelOpinionGroups = {
-  type: "upload-excel-opinion-groups";
-  payload: OpinionsGroup[];
-};
-
 type UploadProfileImage = {
-  type: "upload-profile-image";
+  type: 'upload-profile-image';
   payload: string;
 };
 
 type UpdateCoverOpinion = {
-  type: "update-cover-opinion";
+  type: 'update-cover-opinion';
   payload: string;
 };
 
 type UpdateProfileOpinion = {
-  type: "update-profile-opinion";
+  type: 'update-profile-opinion';
   payload: string;
 };
 
 export type OpinionsActions =
-  //   | Reset
-  | UploadExcelOpinionGroups
   | UploadProfileImage
   | UpdateCoverOpinion
   | UpdateProfileOpinion;
