72f7578a64e221bc948a9360ba39258edb1c3c96
Amos <asidelnik@gmail.com>
2024-03-26 15:02:29 +0200

    RootLayout - replace Context opinions provider with Redux store provider

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -3,17 +3,18 @@ import { Outlet } from "react-router-dom";
 import RootHeader from "../headers/RootHeader";
 import { NavigationProvider } from "../../contexts/navigation-context";
 import { DesignProvider } from "../../contexts/design-context";
-import { OpinionsProvider } from "../../contexts/opinions-context";
 import { ThemeProvider } from "@emotion/react";
 import { headerTheme, bodyTheme } from "../../mui-styles/mui-themes";
 import RootFooter from "../footers/RootFooter";
+import { Provider } from "react-redux";
+import store from '../../redux/store';
 
 export default function RootLayout() {
   return (
     <>
       <NavigationProvider>
         <DesignProvider>
-          <OpinionsProvider>
+          <Provider store={store}>
             <ThemeProvider theme={bodyTheme}>
               <div className="container">
                 <ThemeProvider theme={headerTheme}>
@@ -23,7 +24,7 @@ export default function RootLayout() {
               </div>
               <RootFooter />
             </ThemeProvider>
-          </OpinionsProvider>
+          </Provider>
         </DesignProvider>
       </NavigationProvider>
     </>
